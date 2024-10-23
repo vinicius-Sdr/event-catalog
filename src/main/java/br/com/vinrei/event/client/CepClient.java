c6fc5b8f960f5f4828239b7673025349f1aaa6a3
vinicius-Sdr <vini.santana.612@gmail.com>
2024-10-23 09:41:22 -0300

    wiremock config

--- src/main/java/br/com/vinrei/event/client/CepClient.java
+++ src/main/java/br/com/vinrei/event/client/CepClient.java
@@ -5,10 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "cepClient", url = "viacep.com.br/ws")
+@FeignClient(name = "cepClient", url = "${external.url.api}")
 public interface CepClient {
 
-    @GetMapping("/{cep}/json")
+    @GetMapping("/{cep}")
     CepResponse getCepData(@PathVariable("cep") String cep);
 
 }
